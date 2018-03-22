ced49e67570a5b8dcb7032298db15feeca7d28cc
Pinkie Pie <yarnovax@gmail.com>
2018-03-22 18:50:15 -0400

    Update Noctenium abnormalities

--- config/skills.js
+++ config/skills.js
@@ -1484,8 +1484,14 @@ module.exports = {
 				abnormals: {
 					902: { speed: 1.15 },
 					911: { speed: 1.15 },
+					912: { speed: 1.15 },
+					913: { speed: 1.15 },
 					916: { speed: 1.15 },
-					920: { speed: 1.225 }
+					917: { speed: 1.15 },
+					920: { speed: 1.225 },
+					921: { speed: 1.225 },
+					922: { speed: 1.225 },
+					999010000: { speed: 1.15 }
 				}
 			}
 		},
@@ -1606,8 +1612,14 @@ module.exports = {
 				abnormals: {
 					902: { speed: 1.15 },
 					911: { speed: 1.15 },
+					912: { speed: 1.15 },
+					913: { speed: 1.15 },
 					916: { speed: 1.15 },
-					920: { speed: 1.225 }
+					917: { speed: 1.15 },
+					920: { speed: 1.225 },
+					921: { speed: 1.225 },
+					922: { speed: 1.225 },
+					999010000: { speed: 1.15 }
 				}
 			}
 		},
@@ -1643,8 +1655,14 @@ module.exports = {
 				abnormals: {
 					902: { speed: 1.15 },
 					911: { speed: 1.15 },
+					912: { speed: 1.15 },
+					913: { speed: 1.15 },
 					916: { speed: 1.15 },
-					920: { speed: 1.225 }
+					917: { speed: 1.15 },
+					920: { speed: 1.225 },
+					921: { speed: 1.225 },
+					922: { speed: 1.225 },
+					999010000: { speed: 1.15 }
 				}
 			}
 		},
@@ -1890,8 +1908,14 @@ module.exports = {
 				abnormals: {
 					902: { speed: 1.25 },
 					911: { speed: 1.25 },
+					912: { speed: 1.25 },
+					913: { speed: 1.25 },
 					916: { speed: 1.25 },
-					920: { speed: 1.375 }
+					917: { speed: 1.25 },
+					920: { speed: 1.375 },
+					921: { speed: 1.375 },
+					922: { speed: 1.375 },
+					999010000: { speed: 1.25 }
 				}
 			}
 		},
@@ -2476,7 +2500,7 @@ module.exports = {
 	},
 	9: { // Gunner
 		'*': { consumeAbnormal: [10152010, 10152011] },
-		1: { // Blast
+		/*1: { // Blast
 			'*': {
 				fixedSpeed: 1,
 				length: 1195,
@@ -2489,9 +2513,10 @@ module.exports = {
 			20: {
 				type: 'userProjectile',
 				flyingSpeed: 800,
-				flyingDistance: 500
+				flyingDistance: 500,
+				explodeOnHit: true
 			}
-		},
+		},*/
 		2: { // Bombardment
 			'*': { noRetry: true },
 			0: {
